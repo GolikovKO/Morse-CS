24b7603217cb789f2e92cb27517279015992a1d3
Konstantin <kostya.battlefield13@mail.ru>
2019-04-04 18:32:53 +0400

    First release

---
+++ Morse-CS/MorseDictionary.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class MorseDictionary
+    {
+        public static Dictionary<string, string> data;
+
+        public static void init()
+        {
+            data = new Dictionary<string, string>();
+            data.Clear();  
+
+            data.Add("A", "*-");
+            data.Add("B", "-***");
+            data.Add("C", "-*-*");
+            data.Add("D", "-**");
+            data.Add("E", "*");
+            data.Add("F", "**-*");
+            data.Add("G", "--*");
+            data.Add("H", "****");
+            data.Add("I", "**");
+            data.Add("J", "*---");
+            data.Add("K", "-*-");
+            data.Add("L", "*-**");
+            data.Add("M", "--");
+            data.Add("N", "-*");
+            data.Add("O", "---");
+            data.Add("P", "*--*");
+            data.Add("Q", "--*-");
+            data.Add("R", "*-*");
+            data.Add("S", "***");
+            data.Add("T", "-");
+            data.Add("U", "**-");
+            data.Add("V", "***-");
+            data.Add("W", "*--");
+            data.Add("X", "-**-");
+            data.Add("Y", "-*--");
+            data.Add("Z", "**--");
+
+            data.Add("0", "-----");
+            data.Add("1", "*----");
+            data.Add("2", "**---");
+            data.Add("3", "***--");
+            data.Add("4", "****-");
+            data.Add("5", "*****");
+            data.Add("6", "-****");
+            data.Add("7", "--***");
+            data.Add("8", "---**");
+            data.Add("9", "----*");
+
+            data.Add("!", "--**--");
+            data.Add("?", "**--**");
+            data.Add("-", "=****-");
+            data.Add(",", "*-*-*-");
+            data.Add(";", "-*-*-*");
+
+        }
+    }
+}
---
+++ Morse-CS/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            MorseDictionary.init();
+
+            while (true)
+            {
+                Console.Write("Type your's sentence: ");
+
+                string sentence;
+
+                sentence = Console.ReadLine();
+
+                for (int i = 0; i < sentence.Length; i++)
+                {
+                    if (sentence[i] == ' ')
+                    {
+                        Thread.Sleep(70);
+                        continue;
+                    }
+
+                    var z = MorseDictionary.data[sentence[i].ToString().ToUpper()];
+
+                    Console.Write(z + " ");
+
+                    for (int j = 0; j < z.Length; j++)
+                    {
+                        if (z[j] == '*')
+                        {
+                            Console.Beep(700, 100);
+                        }
+                        else
+                        {
+                            Console.Beep(700, 200);
+                        }
+                        Thread.Sleep(20);
+                    }
+                    Thread.Sleep(70);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
